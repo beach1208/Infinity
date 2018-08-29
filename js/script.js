1eafde70749ff170ebfc844558b017d0d46a9e41
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-08-29 12:32:45 -0700

    animation added

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ $(function () {
 });
 
 
+
 window.sr = ScrollReveal({
     reset: false,
     origin: 'right',
@@ -81,8 +82,35 @@ sr.reveal('.column', {
 });
 
 sr.reveal('.plan-box', {
-    delay: 600,
+    delay: 300,
     origin: 'top',
     easing: 'ease-in-out',
     duration: 500,
+});
+
+$(document).ready(function () {
+    $(".animsition").animsition({
+        inClass: 'zoom-in-sm',
+        outClass: 'zoom-out-sm',
+        inDuration: 800,
+        outDuration: 800,
+        linkElement: '.animsition-link',
+        // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+        loading: true,
+        loadingParentElement: 'body', //animsition wrapper element
+        loadingClass: 'animsition-loading',
+        loadingInner: '', // e.g '<img src="loading.svg" />'
+        timeout: false,
+        timeoutCountdown: 5000,
+        onLoadEvent: true,
+        browser: ['animation-duration', '-webkit-animation-duration'],
+        // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+        // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+        overlay: false,
+        overlayClass: 'animsition-overlay-slide',
+        overlayParentElement: 'body',
+        transition: function (url) {
+            window.location.href = url;
+        }
+    });
 });
